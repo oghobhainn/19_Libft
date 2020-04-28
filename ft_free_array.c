3d4a3ecef5127edd81a3b7d3881e1060bf8a51b7
cmcgahan <chmcgahan@gmail.com>
2020-04-28 15:16:10 +0200

    to free an array

---
+++ ft_free_array.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	ft_free_array(char **array)
+{
+	int	i:
+
+	i = 0;
+	while(array[i])
+	{
+		free(array[i]);
+		array[i] = NULL;
+		i++;
+	}
+	free(array);
+	array = NULL;
+}
